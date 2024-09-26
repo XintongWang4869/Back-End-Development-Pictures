36ad7a22b79979fee18c9fd6805ed1331bc18314
XintongWang4869 <xintong3248@gmail.com>
2024-09-26 00:09:41 -0400

    pictures get, post not finished

--- backend/routes.py
+++ backend/routes.py
@@ -35,7 +35,7 @@ def count():
 ######################################################################
 @app.route("/picture", methods=["GET"])
 def get_pictures():
-    pass
+    return jsonify(data)
 
 ######################################################################
 # GET A PICTURE
@@ -44,7 +44,10 @@ def get_pictures():
 
 @app.route("/picture/<int:id>", methods=["GET"])
 def get_picture_by_id(id):
-    pass
+    for picture in data:
+        if picture['id'] == id:
+            return picture
+    return {"message":"Picture not found."}, 404
 
 
 ######################################################################
@@ -52,7 +55,18 @@ def get_picture_by_id(id):
 ######################################################################
 @app.route("/picture", methods=["POST"])
 def create_picture():
-    pass
+    new_pic = request.json
+    # if not new_pic:
+        # return {"message":"Invalid input parameter"}, 422
+    for picture in data:
+        if picture["id"] == new_pic["id"]:
+            return {"Message": f"picture with id {new_pic['id']} already present"}, 302
+    try:
+        data.append(new_pic)
+        return {"Message": "New picture created"}, 201
+    except NameError:
+        return {"message": "Data not defined"}, 500
+    
 
 ######################################################################
 # UPDATE A PICTURE
